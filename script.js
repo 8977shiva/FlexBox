ba7f6e0339cf8e73b5f65e812ca0f1a66dcc5d68
8977shiva <b.skreddy1997@gmail.com>
2020-06-05 02:20:26 +0530

    flex container

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
-    
-    $('#flex').click(function (e) { 
-        e.preventDefault();
-        $('ul').css('display','flex')
-        alert("display flex added")
-    });
-    $('#flex-d').change((e)=>{
-        let direction=$("#flex-d").val();
-        console.log(direction)
-        $('ul').css('flex-direction',direction)
-    })
+  $("#flex").click(function (e) {
+    e.preventDefault();
+    $("ul").css("display", "flex"); // display:flex
+    alert("display flex added");
+  });
+  $("#flex-d").change((e) => {
+    let direction = $("#flex-d").val();
+    console.log(direction);
+    $("ul").css("flex-direction", direction); // flex-drection:
+  });
+  $("#flex-w").change((e) => {
+    let wrap = $("#flex-w").val();
+    console.log(wrap);
+    $("ul").css("flex-wrap", wrap); // flex-wrap:
+  });
+  $("#flex-f").click(() => {
+    $("ul").css("flex-flow", "row wrap"); // flex-flow:
+  });
+  $('#jc').change(()=>{
+      let content=$('#jc').val();
+    //   console.log(content)
+      $('ul').css("justify-content", content);
+
+  });
+  
+  $('#ai').change(()=>{
+       let  align=$('#ai').val();
+      console.log(align)
+      $('ul').css("align-items", align)
+  })
 });
